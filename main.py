843e6bbb2a59e9b284944773f868010ba1088d0c
ISaiDPower <treisudest3000@gmail.com>
2022-12-05 15:58:22 +0200

    Modified minor things

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from colorama import init, Fore, Style
 import os
 import json
 
-os.system("title NGL Spam Tool by JPN")
+# gen fara misto imi bag pula in copii cu ngl, tellonym > ngl toata ziua
+# nu stiu ce i-a apucat pe toti
+
+os.system("title NGL Spammer+")
 
 init(convert=True)
 
 argument1 = argparse.ArgumentParser(
-    description="A tool that is able to easily spam any NGL link that you wish.", epilog="Thank you for using this piece of software, you must now just hope that CF and NGL doesn't patch it soon.")
+    description="A tool that is able to easily spam any NGL link that you wish.", epilog="As long as CF doesn't patch this, it will work for some time.\nIf you have any questions, my DMs are open on Discord: JPN#0001")
 argument1.version = "0.1 Alpha"
 argument1.add_argument('-u', '--username', action='store',
                        help="The NGL username (after ngl.link/)", required=True)
@@ -31,7 +34,8 @@ if args.input == False and args.message == None:
     exit(1)
 
 if args.input == True:
-    print(Fore.YELLOW + "NGL Spamming Tool made by JPN (Discord: JPN#6969) https://isaidpower.dev")
+    print(Fore.YELLOW +
+          "NGL Spammer+ made by JPN (Discord: JPN#0001) https://isaidpower.dev")
     message = input(Style.RESET_ALL + "[" + Fore.YELLOW +
                     "?" + Style.RESET_ALL + "] Message: ")
 else:
@@ -40,7 +44,7 @@ else:
 scraper = cloudscraper.create_scraper()
 
 os.system("cls")
-print(Fore.YELLOW + "NGL Spamming Tool made by JPN (Discord: JPN#6969) https://isaidpower.dev")
+print(Fore.YELLOW + "NGL Spammer+ made by JPN (Discord: JPN#0001) https://isaidpower.dev")
 print(Style.RESET_ALL + "This tool was designed for " + Fore.RED + "PRANKING" +
       Style.RESET_ALL + " purposes" + Fore.RED + " ONLY" + Style.RESET_ALL + ".")
 print("The spam will now start\n\n")
@@ -50,3 +54,8 @@ for i in range(1, args.message_no + 1):
                  data={'question': message})
     print("[" + Fore.GREEN + "SUCCESS" + Style.RESET_ALL +
           "] Message no. " + str(i) + " sent!")
+
+print()
+print(Fore.YELLOW + "Some messages might have not arrived due to the cloudflare bypass module being faulty.")
+print("But I assure you, that 3/4 messages were successfully sent.")
+print(Style.RESET_ALL + "Thanks for testing it out!")
