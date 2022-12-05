383bee8615d264fa8b05f16b064c55c8a88c73c7
ISaiDPower <treisudest3000@gmail.com>
2022-12-05 17:32:51 +0200

    Final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ast import For, arg
+from time import sleep
 from xml.sax.handler import version
 import cloudscraper
 import argparse
@@ -15,7 +16,7 @@ init(convert=True)
 
 argument1 = argparse.ArgumentParser(
     description="A tool that is able to easily spam any NGL link that you wish.", epilog="As long as CF doesn't patch this, it will work for some time.\nIf you have any questions, my DMs are open on Discord: JPN#0001")
-argument1.version = "0.1 Alpha"
+argument1.version = "0.2 Final"
 argument1.add_argument('-u', '--username', action='store',
                        help="The NGL username (after ngl.link/)", required=True)
 argument1.add_argument('-n', '--message_no', action='store',
@@ -49,11 +50,15 @@ print(Style.RESET_ALL + "This tool was designed for " + Fore.RED + "PRANKING" +
       Style.RESET_ALL + " purposes" + Fore.RED + " ONLY" + Style.RESET_ALL + ".")
 print("The spam will now start\n\n")
 
+# q: how much to wait for cloudflare rate limit
+
+
 for i in range(1, args.message_no + 1):
     scraper.post("https://ngl.link/" + args.username,
                  data={'question': message})
     print("[" + Fore.GREEN + "SUCCESS" + Style.RESET_ALL +
-          "] Message no. " + str(i) + " sent!")
+          "] Message no. " + str(i) + " sent! And waiting 5 seconds...")
+    sleep(5)
 
 print()
 print(Fore.YELLOW + "Some messages might have not arrived due to the cloudflare bypass module being faulty.")
